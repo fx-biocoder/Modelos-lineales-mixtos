219a3470d9f1c6730246ba8e25a9b2d702cd80d1
Facundo Martínez <90737264+fx-biocoder@users.noreply.github.com>
2021-12-08 19:43:00 -0300

    Archivos principales

---
+++ MLM - Arañas - lme4/Modelo lineal mixto - Arañas.r
@@ -0,0 +1,81 @@
+# 1 - Librerías
+library(lme4)
+
+# 2 - Función de qq-plot
+qq_plotting =  function(model){
+  plot(model)
+  qqnorm(resid(model))
+  qqline(resid(model), col = "blue")
+  shapiro.test(residuals(model))
+}
+
+# 3 - Análisis del caso
+#     Wolff et al. (2020) estudiaron la forma y adhesión de las telarañas
+#     en función del microambiente donde se encuentran las especies. En base a eso
+#     desarrollaron modelos lineales mixtos para mostrar la relación entre distintas
+#     variables ambientales. Este script utiliza los datos experimentales de los
+#     autores para desarrollar el mismo modelo de forma independiente.
+
+# Nota: los dataframes 114_2020_1687_MOESM1_ESM.csv y 114_2020_1687_MOESM2_ESM.csv
+# se han importado con los nombres M1 y M2 respectivamente, por una cuestión de simplicidad
+
+# Elaboración del modelo:
+modelo1.araña <- lmer(force ~ spec + subs + (ind|spec), M1)
+summary(modelo1.araña)
+qq_plotting(modelo1.araña)
+# El modelo no es normal. 
+
+# Transformación logarítmica para obtener normalidad y homocedasticidad:
+M1$logForce <- log(M1$force)
+modelo2.araña <- lmer(logForce ~ spec + subs + (ind|spec), M1)
+qq_plotting(modelo2.araña) # Ahora hay normalidad y homocedasticidad
+
+# Se procede a visualizar si el ajuste mejora al remover la variable especie
+modelo3.araña <- lmer(logForce ~ subs + (ind|spec), M1)
+modelo4.araña <- lmer(logForce ~ subs + (1|ind), M1)
+AIC(modelo2.araña, modelo3.araña, modelo4.araña)
+
+# Se concluye que la variable especie no influye en la fuerza de adhesión
+
+# ¿Hay influencia de la química del sustrato?
+modelo5.araña <- lmer(logForce ~ 1 + (1|ind), M1)
+AIC(modelo2.araña, modelo3.araña, modelo4.araña, modelo5.araña)
+
+# El Akaike empeora, por ende hay influencia de la química del substrato
+
+# Transformación logarítmica de la relación ancho-largo
+M2$logRelwid <- log(M2$relwid)
+
+# Modelo mixto para la relación ancho-largo utilizando todas las variables
+modeloWL_m2 <- lmer(logRelwid ~ spec + subs + frontshift + centrality + (ind|spec), M2)
+qq_ploting(modeloWL_m2)
+# El modelo es normal y homocedástico
+
+# Creación de modelos anidados
+modeloWL_m2 <- lmer(logRelwid ~ spec + subs + frontshift + centrality + (ind|spec), M2)
+modelo2WL_m2 <- lmer(logRelwid ~ subs + frontshift + centrality + (ind|spec), M2)
+modelo3WL_m2 <- lmer(logRelwid ~ subs + frontshift + centrality + (1|ind), M2)
+modelo4WL_m2 <- lmer(logRelwid ~ frontshift + centrality + (1|ind), M2)
+modelo5WL_m2 <- lmer(logRelwid ~ frontshift + (1|ind), M2)
+
+AIC(modeloWL_m2, modelo2WL_m2, modelo3WL_m2, modelo4WL_m2, modelo5WL_m2)
+
+# Se concluye que el modelo 4 es el mejor con respecto a la relación ancho-largo
+
+# Creación de un modelo para la variable área:
+modeloA_m2 <- lmer(area ~ spec + subs + frontshift + centrality + (ind|spec), M2)
+qq_plotting(modeloA_m2)
+
+# El modelo no es normal. Se procede a realizar una transformación logarítmica:
+M2$logArea <- log(M2$area)
+modeloA_m2 <- lmer(logArea ~ spec + subs + frontshift + centrality + (ind|spec), M2)
+qq_ploting(modeloA_m2)
+
+# Ahora que se obtuvo normalidad, se procede a elaborar modelos anidados
+modeloA2_m2 <- lmer(logArea ~ subs + frontshift + centrality + (ind|spec), M2)
+modeloA3_m2 <- lmer(logArea ~ frontshift + centrality + (1|ind), M2)
+modeloA4_m2 <- lmer(logArea ~ frontshift + (1|ind), M2)
+AIC(modeloA_m2, modeloA2_m2, modeloA3_m2, modeloA4_m2)
+
+# Vemos que el mejor modelo es el 3
+# Tanto el área como la relación ancho.largo dependen de las mismas variables. No existen diferencias.
---
+++ MLM - Hueso ramus - NLME/Modelo lineal mixto - Hueso ramus.r
@@ -0,0 +1,71 @@
+# 1 - Librerías necesarias para el proyecto
+library(nlme)
+library(lme4)
+
+# 2 - Funciones necesarias
+boxplot_graph = function(dataframe, aes_x, aes_y){
+  library(ggplot2)
+  ggplot(dataframe,
+         aes(x = aes_x,
+             y = aes_y)) +
+    geom_boxplot() +
+    stat_boxplot(geom = "errorbar")
+}
+
+qq_plotting =  function(model){
+  plot(model)
+  qqnorm(resid(model))
+  qqline(resid(model), col = "blue")
+  shapiro.test(residuals(model))
+}
+
+# 3 - Análisis del caso
+#     El dataframe Hueso.csv contiene información sobre la longitud del hueso
+#     ramus para 20 niños a lo largo de un período de 2 años y medio. Comenzamos por tomar como
+#     factor los individuos
+Hueso$fIndividuo <- factor(Hueso$individuo)
+modelo <- lme(HuesoRamus ~ edad, random = ~1 | fIndividuo, data = Hueso)
+summary(modelo)
+
+# Ploteamos el modelo ajustado:
+F0 <- fitted(modelo, level = 0)
+I <- order(Hueso$edad)
+edades <- sort(Hueso$edad)
+
+plot(edades, 
+     F0[I], 
+     lwd  = 4, 
+     type = "l", 
+     ylim = c(44,57), 
+     ylab = "HuesoRamus", 
+     xlab = "edad")
+
+F1 <- fitted(modelo, level = 1)
+
+for (i in 1:20){
+  x1 <- Hueso$edad[Hueso$fIndividuo == i]
+  y1 <- F1[Hueso$fIndividuo == i]
+  K  <- order(x1)
+  lines(sort(x1), y1[K])
+}
+
+x = text(Hueso$edad, Hueso$HuesoRamus, Hueso$fIndividuo, cex=0.7)
+
+# 4 - Establecimiento del modelo
+#     Se usarán dos métodos distintos para el cálculo del cociente de verosimilitud:
+#     el criterio normal (ML) y el restringido (REML)
+Hueso$logHuesoRamus <- log(Hueso$HuesoRamus)
+Hueso$fEdad <- factor(Hueso$edad)
+modelo.ml <- lme(HuesoRamus ~ edad, data = Hueso, random = ~1| fIndividuo, method = "ML")
+modelo.reml <-lme(HuesoRamus ~ edad, data = Hueso, random = ~1 | fIndividuo, method = "REML")
+
+# Uso del Akaike Information Criterion para determinar cuál es el mejor modelo
+AIC(modelo.ml, modelo.reml)
+
+# 5 - ¿Hay normalidad y homocedasticidad?
+boxplot_modelo.ml <- boxplot_graph(Hueso, Hueso$fEdad, Hueso$HuesoRamus)
+boxplot_modelo.ml
+# El boxplot nos indica que no podemos afirmar ausencia de homocedasticidad con seguridad
+# por lo que hacemos un qq-plot con la función creada previamente
+qq_plotting(modelo.ml)
+# No hay normalidad
